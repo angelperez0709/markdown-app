27129c8a2a9e01494120b167dbb8b7c3e595ca72
Angel <angelperez07092000@gmail.com>
2026-01-31 17:54:50 +0100

    create main

--- cmd/markdown-app/main.go
+++ cmd/markdown-app/main.go
@@ -1,6 +1,27 @@
 package main
 
+import (
+	"log"
+	"time"
+
+	"github.com/angelperez0709/markdown-app/internal/app"
+	"github.com/angelperez0709/markdown-app/internal/config"
+)
 
 func main(){
-	
+	cfg := config.Config{
+		Addr: ":8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout: 15 * time.Second,
+		IdleTimeout: 60 * time.Second,
+	}
+	app := app.Application{
+		Config: cfg,
+	}
+	handler := app.Mount()
+	if err := app.Run(handler); err != nil {
+		log.Printf("An error ocurred %s",err.Error())
+		panic(err)
+	}
+
 }
